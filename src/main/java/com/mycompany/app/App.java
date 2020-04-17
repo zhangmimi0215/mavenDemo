586b2b95deed815b2c8aea36f690a6a918ad5246
zhangmimi0215 <40972570+zhangmimi0215@users.noreply.github.com>
2020-04-17 08:54:14 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
 
-        System.out.println("A NullPointerException is a drag...");
+        System.out.println("A NullPointerException is a drag...  测试更改");
         Object myObject = null;
         /**
          * Checker prevents this from compiling...
